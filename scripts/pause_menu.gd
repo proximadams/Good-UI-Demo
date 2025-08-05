3cb05caaf22f9b502492aa791e267c1e33941385
Lucky Adams <proximadams@gmail.com>
2025-08-05 17:40:41 -0500

    clicking increase/decrease adjusts sliders

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -1,4 +1,15 @@
 extends Control
 
+@export var sliderArr: Array[Control]
+
 func _on_quit_game_button_pressed() -> void:
 	get_tree().quit()
+
+func _incdec_slider(sliderIndex: int, increase: bool):
+	print('sliderArr.size() = ' + str(sliderArr.size()))
+	if 0 <= sliderIndex and sliderIndex < sliderArr.size():
+		var currSlider = sliderArr[sliderIndex]
+		if increase:
+			currSlider.value += currSlider.step
+		else:
+			currSlider.value -= currSlider.step

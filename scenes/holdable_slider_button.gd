2fb4b31c8cb8ce4c235ca5c34715e12ef9ce4bdb
Lucky Adams <proximadams@gmail.com>
2025-08-05 22:18:39 -0500

    holdable slider buttons and tooltip resizing

---
+++ scenes/holdable_slider_button.gd
@@ -0,0 +1,37 @@
+extends Button
+
+const REFIRE_WAIT_TIME: float = 0.5
+const REFIRE_STEP_TIME: float = 0.05
+
+@export var slider: HSlider
+
+var heldTimer: float = 0.0
+var heldCount: int = 0
+
+@onready var increase: bool = _check_is_button_more()
+
+func _ready() -> void:
+	self.connect('button_down', _on_button_down)
+	self.connect('button_up', _on_button_up)
+
+func _process(delta: float) -> void:
+	if 0.0 < heldTimer:
+		var oldHeldTimer = heldTimer
+		var nextWaitTime = REFIRE_WAIT_TIME + (heldCount * REFIRE_STEP_TIME)
+		heldTimer += delta
+		if oldHeldTimer < nextWaitTime and nextWaitTime <= heldTimer:
+			heldCount += 1
+			if increase:
+				slider.value += slider.step
+			else:
+				slider.value -= slider.step
+
+func _on_button_down():
+	heldTimer = 0.00001
+
+func _on_button_up():
+	heldTimer = 0.0
+	heldCount = 0
+
+func _check_is_button_more():
+	return 'More' in self.name
--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@export var sliderArr: Array[Control]
+@export var sliderArr: Array[HSlider]
 
 var uiUtil = preload('res://scripts/ui_util.gd').new()
 
@@ -16,11 +16,27 @@ const MIN_UI_SCALE = 0.2
 
 func _ready() -> void:
 	_refresh_ui_scale()
+	_connect_tooltip_signals()
+
+func _get_children_recursive_array(parentNode: Control) -> Array[Control]:
+	var result: Array[Control] = []
+	if parentNode.get_child_count() != 0:
+		var children = parentNode.get_children()
+		result.append_array(children)
+		for currChild in children:
+			result.append_array(_get_children_recursive_array(currChild))
+	return result
+
+func _connect_tooltip_signals() -> void:
+	var childrenRecursiveArr = _get_children_recursive_array(self)
+	for currChild in childrenRecursiveArr:
+		if currChild.tooltip_text != '':
+			currChild.connect('child_entered_tree', try_tooltip_resize)
 
 func _on_quit_game_button_pressed() -> void:
 	tree.quit()
 
-func _incdec_slider(sliderIndex: int, increase: bool):
+func _incdec_slider(sliderIndex: int, increase: bool) -> void:
 	if 0 <= sliderIndex and sliderIndex < sliderArr.size():
 		var currSlider = sliderArr[sliderIndex]
 		if increase:
@@ -30,9 +46,12 @@ func _incdec_slider(sliderIndex: int, increase: bool):
 		if sliderIndex == UI_SCALE_SLIDER_INDEX:
 			_refresh_ui_scale()
 
-func _refresh_ui_scale():
+func _get_ui_scale_value() -> float:
 	var uiScaleSlider = sliderArr[UI_SCALE_SLIDER_INDEX]
-	var scaleValue = MIN_UI_SCALE + (1.0 - MIN_UI_SCALE)*(uiScaleSlider.value / uiScaleSlider.max_value)
+	return MIN_UI_SCALE + (1.0 - MIN_UI_SCALE)*(uiScaleSlider.value / uiScaleSlider.max_value)
+
+func _refresh_ui_scale() -> void:
+	var scaleValue = _get_ui_scale_value()
 	for currControl in scalableControls:
 		currControl.scale = Vector2(scaleValue, scaleValue)
 
@@ -41,3 +60,10 @@ func _input(event: InputEvent) -> void:
 	if is_instance_valid(focusOwner) and focusOwner.name == 'UiScaleHSlider' \
 	and (uiUtil._check_was_ui_movement_input(event) or uiUtil._check_was_left_mouse_click(event)):
 		_refresh_ui_scale()
+
+func try_tooltip_resize(node: Node) -> void:
+	if node is PopupPanel:
+		var scaleValue: float = _get_ui_scale_value()
+		var children = node.get_children(true)
+		for currChild in children:
+			currChild.scale = Vector2(scaleValue, scaleValue)
